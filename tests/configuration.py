dc2be006000e484d050a66aa73fdd40c003ccfa1
jhickmanarc <jhickman@arcdigitalservices.com>
2024-01-02 16:14:10 -0800

    Unit test for the configuration loader

--- tests/configuration.py
+++ tests/configuration.py
@@ -1,3 +1,25 @@
+import unittest
+import os
+
+from chordataweb.configuration import env_loader, dynamic_key_loader
+
 """
 Automated tests for the configuration module
 """
+
+
+class TestConfiguration(unittest.TestCase):
+    def test_environment_variable(self):
+        os.environ['CHOR_test'] = "test value"
+        os.environ['CHOR_test_2'] = "test value2"
+        result = env_loader(['test', 'test_2'])
+        assert result['test'] == 'test value'
+        assert result['test_2'] == 'test value2'
+
+    def test_custom_keys_loader(self):
+        os.environ['CHOR_test'] = "test value"
+        os.environ['CHOR_test_2'] = "test value2"
+        os.environ['CHOR_custom_keys'] = "test:test_2"
+        result = env_loader([])
+        assert result['test'] == 'test value'
+        assert result['test_2'] == 'test value2'
